4a94a8f6f068c02a75f605b4454be8e0d8f49474
Vikas Ghunawat <53647611+vikghunawat@users.noreply.github.com>
2020-11-08 23:32:39 +0530

    Football Site

---
+++ one.js
@@ -0,0 +1,16 @@
+$('#nav').fadeIn(500,function() {
+    $('#main').fadeIn(1000,function() {
+       $('#footer').fadeIn(1000);     
+    });
+});​
+
+// jQuery(function ($) {
+//     $('.loading').show();
+//     $('.main').hide();
+// });
+// $(document).on('load', function () {
+//     $('.main').fadeIn(500);
+//     $('.loading').hide();
+// });
+
+
